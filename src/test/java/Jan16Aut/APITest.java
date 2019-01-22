bf05c9963a1f936ebef0b5d4f9d4acd4c7d2acd8
lucky17166 <lucky17166@gmail.com>
2019-01-22 15:04:04 -0300

    This is my first commit

---
+++ src/test/java/Jan16Aut/APITest.java
@@ -0,0 +1,18 @@
+package Jan16Aut;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	 @Test
+	 public void APITest1()
+	 {
+		 System.out.println("APITest1");
+	 }
+	 
+	 @Test
+	 public void APITest2()
+	 {
+		 System.out.println("APITest2");
+	 }
+		
+}
---
+++ src/test/java/Jan16Aut/Selenium.java
@@ -0,0 +1,18 @@
+package Jan16Aut;
+
+import org.testng.annotations.Test;
+
+public class Selenium {
+     @Test
+	 public void Sel1()
+	 {
+		 System.out.println("In selenium class selinum 1 method");
+	 }
+	 
+	 @Test
+	 public void Sel2()
+	 {
+		 System.out.println("In selenium class selinum 2 method");
+	 }
+     	
+}
